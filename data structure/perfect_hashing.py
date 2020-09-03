c953b0fcb2bfc1eed9190414a928ee39bf793ad5
Peerapon <peerapon.ppw@gmail.com>
2020-09-03 19:24:53 +0700

    update perfect hash

--- data structure/perfect_hashing.py
+++ data structure/perfect_hashing.py
@@ -1,4 +1,21 @@
+import random
+""" Example from Figure 11.6"""
+class Node:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
 
-T = [[] for _ in range(10)]
+T = [[] for _ in range(9)] # hash table with chaining
+K = [10,22,37,40,52,60,70,72,75] # set key
+
+def h(k):
+    a = random.uniform(min(K),max(K))
+    b = random.uniform(min(K),max(K))
+    p = 101 # p is not random (you choose 1 prime number) and p > m
+    return int((((a*k) + b) % p) % len(T))
+
+def h_insert(T,x):
+    h1 = h(x.key)
 
 
+print(h(75))
